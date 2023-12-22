22ed69752656c571c04f2943c9a3cd90c3693e3e
parkseokwon <parkswon1@naver.com>
2023-12-22 21:03:04 +0900

    객체 지향

--- Myapp/AccountingApp.java
+++ Myapp/AccountingApp.java
@@ -1,37 +1,52 @@
-
+class Accounting{
+    public static double valueOfSupply;
+    public static double vatRate;
+    public static double expenseRate;
+    public static void print() {
+        System.out.println("Value of supply : " + valueOfSupply);
+        System.out.println("VAT : " + getVAT());
+        System.out.println("Total : " + getTotal());
+        System.out.println("Expense : " + getExpense());
+        System.out.println("Income : " + getIncome());
+        System.out.println("Dividend 1 : " + getDiviend1());
+        System.out.println("Dividend 2 : " + getDiviend2());
+        System.out.println("Dividend 3 : " + getDiviend3());
+    }
+ 
+    public static double getDiviend1() {
+        return getIncome() * 0.5;
+    }
+    public static double getDiviend2() {
+        return getIncome() * 0.3;
+    }
+    public static double getDiviend3() {
+        return getIncome() * 0.2;
+    }
+ 
+    public static double getIncome() {
+        return valueOfSupply - getExpense();
+    }
+ 
+    public static double getExpense() {
+        return valueOfSupply * expenseRate;
+    }
+ 
+    public static double getTotal() {
+        return valueOfSupply + getVAT();
+    } 
+ 
+    public static double getVAT() {
+        return valueOfSupply * vatRate;
+    }
+}
 public class AccountingApp {
-	public static void main(String[] args) {
-		double supplyCost = Double.parseDouble(args[0]);
-		double VAT = getVAT(supplyCost);
-		double Total = getTotal(supplyCost, VAT);
-		double Expense = supplyCost*0.3;
-		double Income = supplyCost - Expense;
-		double[] Dividend= new double[3];
-		if(Income > 10000) {
-			Dividend[0] = Income/2;
-			Dividend[1] = Income*0.3;
-			Dividend[2] = Income*0.2;
-		}else {
-			Dividend[0] = Income;
-			Dividend[1] = Income*0;
-			Dividend[2] = Income*0;
-		}
-		
-		System.out.println("Value of supply :" + supplyCost);
-		System.out.println("VAT :" + VAT);
-		System.out.println("Total :" + Total);
-		System.out.println("Expense :" + Expense);
-		System.out.println("Income :" + Income);
-		for (int i = 0; i < 3; i++) {
-			System.out.println("Dividend["+i+"] :" + Dividend[i]);
-		}
-	}
-
-	private static double getTotal(double supplyCost, double VAT) {
-		return supplyCost + VAT;
-	}
-
-	private static double getVAT(double supplyCost) {
-		return supplyCost/10;
-	}
+    public static void main(String[] args) {
+        Accounting.valueOfSupply = 10000.0;
+        Accounting.vatRate = 0.1;
+        Accounting.expenseRate = 0.3;
+        Accounting.print();
+        // anotherVariable = ...;
+        // anotherMethod = ...;
+    }
+ 
 }
