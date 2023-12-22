156d54a817a9006097748ad50543b4209adcf8aa
parkseokwon <parkswon1@naver.com>
2023-12-22 20:57:52 +0900

    매소드화 하기

--- Myapp/AccountingApp.java
+++ Myapp/AccountingApp.java
@@ -2,20 +2,36 @@
 public class AccountingApp {
 	public static void main(String[] args) {
 		double supplyCost = Double.parseDouble(args[0]);
-		double VAT = supplyCost/10;
-		double Total = supplyCost + VAT;
+		double VAT = getVAT(supplyCost);
+		double Total = getTotal(supplyCost, VAT);
 		double Expense = supplyCost*0.3;
 		double Income = supplyCost - Expense;
-		double Dividend1 = Income/2;
-		double Dividend2 = Income*0.3;
-		double Dividend3 = Income*0.2;
+		double[] Dividend= new double[3];
+		if(Income > 10000) {
+			Dividend[0] = Income/2;
+			Dividend[1] = Income*0.3;
+			Dividend[2] = Income*0.2;
+		}else {
+			Dividend[0] = Income;
+			Dividend[1] = Income*0;
+			Dividend[2] = Income*0;
+		}
+		
 		System.out.println("Value of supply :" + supplyCost);
 		System.out.println("VAT :" + VAT);
 		System.out.println("Total :" + Total);
 		System.out.println("Expense :" + Expense);
 		System.out.println("Income :" + Income);
-		System.out.println("Dividend :" + Dividend1);
-		System.out.println("Dividend :" + Dividend2);
-		System.out.println("Dividend :" + Dividend3);
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Dividend["+i+"] :" + Dividend[i]);
+		}
+	}
+
+	private static double getTotal(double supplyCost, double VAT) {
+		return supplyCost + VAT;
+	}
+
+	private static double getVAT(double supplyCost) {
+		return supplyCost/10;
 	}
 }
