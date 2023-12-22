ff307ab4963fa7382fab9b275ea684baa9a2a59f
parkseokwon <parkswon1@naver.com>
2023-12-22 20:29:08 +0900

    공급가 계산기

---
+++ Myapp/AccountingApp.java
@@ -0,0 +1,21 @@
+
+public class AccountingApp {
+	public static void main(String[] args) {
+		double supplyCost = 10000.0;
+		double VAT = supplyCost/10;
+		double Total = supplyCost + VAT;
+		double Expense = supplyCost*0.3;
+		double Income = supplyCost - Expense;
+		double Dividend1 = Income/2;
+		double Dividend2 = Income*0.3;
+		double Dividend3 = Income*0.2;
+		System.out.println("Value of supply :" + supplyCost);
+		System.out.println("VAT :" + VAT);
+		System.out.println("Total :" + Total);
+		System.out.println("Expense :" + Expense);
+		System.out.println("Income :" + Income);
+		System.out.println("Dividend :" + Dividend1);
+		System.out.println("Dividend :" + Dividend2);
+		System.out.println("Dividend :" + Dividend3);
+	}
+}
