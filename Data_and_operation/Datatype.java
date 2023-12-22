243a36e563787872d7d90042139b7fbd17da84be
parkseokwon <parkswon1@naver.com>
2023-12-22 17:49:53 +0900

    1개의 역슬래시 를 만들라면 \\2개쓰면된다.

---
+++ Data_and_operation/Datatype.java
@@ -0,0 +1,8 @@
+public class Datatype{
+	public static void main(String[] args) {
+        String backslashString = "This is a backslash: \\";
+
+        // 출력
+        System.out.println(backslashString);
+	}
+}
