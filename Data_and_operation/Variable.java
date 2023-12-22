e079c020fbf125baf30f8d8b3b4305308a2bde0b
parkseokwon <parkswon1@naver.com>
2023-12-22 18:44:14 +0900

    class, 상속, 매소드 인스텐스

--- Data_and_operation/Variable.java
+++ Data_and_operation/Variable.java
@@ -2,14 +2,37 @@
 public class Variable {
 
 	public static void main(String[] args) {
-        int a = 1; // Number -> integer  ... -2, -1 , 0, 1, 2 ...
-        System.out.println(a);
-         
-        double b = 1.1; // real number -> double ... -2.0, -1.0, 0, 1.0, 2.0 ...
-        System.out.println(b);
-         
-        String c = "Hello World";
-        System.out.println(c);
+        Child childO = new Child();
+
+        // 자식 클래스에서 오버라이딩한 메서드 호출
+        childO.display(); // 출력: Child class: 10
+
+        // 자식 클래스에서 멤버 변수 변경
+        childO.changeValue();
+
+        // 변경된 값을 출력
+        childO.display(); // 출력: Child class: 20
 	}
 
+}
+
+class Parent {
+    int value = 10;
+
+    void display() {
+        System.out.println("Parent class: " + value);
+    }
+}
+
+class Child extends Parent {
+    // 부모 클래스에서 상속받은 메서드를 오버라이딩
+    @Override
+    void display() {
+        System.out.println("Child class: " + value);
+    }
+
+    // 부모 클래스에서 상속받은 멤버 변수를 변경
+    void changeValue() {
+        value = 20;
+    }
 }
