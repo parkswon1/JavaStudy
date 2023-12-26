cc03581673ae5720cd7e0eec6bf15f5f15d84df9
parkseokwon <parkswon1@naver.com>
2023-12-26 20:26:16 +0900

    2주차 시작

---
+++ BooleanApp/Boolean.java
@@ -0,0 +1,15 @@
+
+public class Boolean {
+	public static void main(String[] args) {
+		System.out.println("One");
+		System.out.println("two");
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hellow world";
+		
+		System.out.println(foo.contains("world"));
+		System.out.println(foo.contains("egoing"));
+	}
+}
