c466608d5aa14bc3aa6f812d47afa8e08db15082
parkseokwon <parkswon1@naver.com>
2024-01-12 19:39:55 +0900

    this는 class혹은 method가 받아오는 argment들을 칭함

--- OthersOOP/src/MyOOP.java
+++ OthersOOP/src/MyOOP.java
@@ -1,22 +1,39 @@
 package OthersOOP.src;
 
-class Print{
-    public static void printLine() {
-        System.out.println("----");
+class Print {
+    public String delimiter = "";
+    public Print(String delimiter) {
+        this.delimiter = delimiter;
     }
-
-    public static void extracted(String input) {
-        System.out.println(input);
-        System.out.println(input);
+    public void A() {
+        System.out.println(delimiter);
+        System.out.println("A");
+        System.out.println("A");
+    }
+    public void B() {
+        System.out.println(delimiter);
+        System.out.println("B");
+        System.out.println("B");
     }
 }
 public class MyOOP {
     public static void main(String[] args) {
+        Print p1 = new Print("----");
+        p1.A();
+        p1.A();
+        p1.B();
+        p1.B();
 
-        Print.printLine();
-        Print.extracted("A");
+        Print p2 = new Print("****");
+        p2.A();
+        p2.A();
+        p2.B();
+        p2.B();
 
-        Print.printLine();
-        Print.extracted("B");
+
+        p1.A();
+        p2.A();
+        p1.A();
+        p2.A();
     }
 }
