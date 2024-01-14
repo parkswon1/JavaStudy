30b5f42f7c07131d5d6aadb64640ba347b6ac18b
parkseokwon <parkswon1@naver.com>
2024-01-14 22:57:13 +0900

    extend의 장점은 부모의 method들을 가져와서 사용할 수 있다는 점

--- OthersOOP/src/InheritanceApp.java
+++ OthersOOP/src/InheritanceApp.java
@@ -1,29 +1,23 @@
 package OthersOOP.src;
 
-class Cal {
-    public int sum(int v1, int v2) {
-        return v1 + v2;
+class Cal{
+    int v1,v2;
+    Cal(int v1, int v2){
+        System.out.println("Cal init!!");
+        this.v1 = v1; this.v2 = v2;
     }
+
 }
 class Cal3 extends Cal{
-
-    public int mix(int v1, int v2, int v3) {
-        return sum(v1,v2) * minus(v3,v2);
+    Cal3(int v1, int v2) {
+        super(v1, v2);
+        System.out.println("Cal3 init!!");
     }
-
-    public int minus(int v1, int v2) {
-        return v1 - v2;
-    }
+    public int minus(){return this.v1-v2;}
 }
-
 public class InheritanceApp {
-
     public static void main(String[] args) {
-        Cal c = new Cal();
-        System.out.println(c.sum(2, 1));
-        Cal3 c3 = new Cal3();
-        System.out.println(c3.sum(2, 1));
-        System.out.println(c3.minus(2, 1));
-        System.out.println(c3.mix(2,1,3));
+        Cal c = new Cal(2,1);
+        Cal3 c3 = new Cal3(2, 1);
     }
 }
