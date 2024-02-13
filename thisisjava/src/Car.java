f9e486cad474722639aa152de668fa0e38744d7b
parkseokwon <parkswon1@naver.com>
2024-02-13 15:29:07 +0900

    @Getter @Setter 게터 세터 이해하기

--- thisisjava/src/Car.java
+++ thisisjava/src/Car.java
@@ -1,19 +1,11 @@
 public class Car {
-    private int count;
+    private double speed;
 
-    public Car() {
-        this.count = 0;
+    public void setSpeed(double speed){
+        this.speed = speed;
     }
 
-    public void increment() {
-        this.count++; // "this"를 사용하여 현재 객체의 변수에 접근
-    }
-
-    public void reset() {
-        this.count = 0; // "this"를 사용하여 현재 객체의 변수에 접근
-    }
-
-    public int count(){
-        return this.count;
+    public double getSpeed(){
+        return this.speed;
     }
 }
--- thisisjava/src/CarExample.java
+++ thisisjava/src/CarExample.java
@@ -1,8 +1,6 @@
 public class CarExample {
     public static void main(String[] args){
         Car car1 = new Car();
-        car1.increment();
-        car1.reset();
-        System.out.println(car1.count());
+
     }
 }
