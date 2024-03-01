a4c209b859c6965d4da6554d93c8b58fec0f45a5
parkseokwon <parkswon1@naver.com>
2024-03-01 20:39:07 +0900

    @Getter @Setter 게터 세터 이해하기

---
+++ thisisjava/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("hello");
+        System.out.println(list.get(0));
+    }
+}
