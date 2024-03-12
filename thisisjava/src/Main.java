dc0ea18a5f1ff41d3bf82ecf1c870eb1f2d38705
parkseokwon <parkswon1@naver.com>
2024-03-12 09:35:54 +0900

    별표 출력

--- thisisjava/src/Main.java
+++ thisisjava/src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        SmartPhone smartPhone = new SmartPhone("홍길동");
-
-        smartPhone.turnOn();
-        smartPhone.internetSearch();
-        smartPhone.turnOff();
-        System.out.println(smartPhone.owner);
+        System.out.println("김성박");
+        System.out.println("urt@gmail.com");
+        System.out.println("남자");
     }
 }
---
+++ thisisjava/src/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle {
+    public static void main(String[] args){
+        for (Integer i = 0; i < 10; i++){
+            for (Integer j = 0; j < 10; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
