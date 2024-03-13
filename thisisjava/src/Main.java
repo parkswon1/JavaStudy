b66f4eac38d7a6cb5b33df35567078b76804c435
parkseokwon <parkswon1@naver.com>
2024-03-13 13:36:57 +0900

    scanner연습

--- thisisjava/src/Main.java
+++ thisisjava/src/Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("김성박");
-        System.out.println("urt@gmail.com");
-        System.out.println("남자");
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int dev = 2;
+        int check = 0 ;
+        while(dev < input){
+            if (input % dev == 0){
+                check = 1;
+                break;
+            }
+            dev += 1;
+        }
+        if (check == 1){
+            System.out.println(input + "은 소수가 아닙니다.");
+        }else{
+            System.out.println(input + "은 소수입니다.");
+        }
     }
 }
