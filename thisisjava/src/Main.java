faa250e928b955fd7a8d3abc6eb0bbab0ed50f2a
parkseokwon <parkswon1@naver.com>
2024-03-13 23:24:09 +0900

    재귀함수 연습

--- thisisjava/src/Main.java
+++ thisisjava/src/Main.java
@@ -1,11 +1,23 @@
 public class Main {
     public static void main(String[] args){
-        for(int k = 1; k <= 9; k++) {
-            for (int i = 1; i <= 9; i++) {
-                System.out.print(k + " * " + i + " = " + (k * i)+" | ");
-            }
-            System.out.println();
+        factorial1(1,1);
+        System.out.println(factorial2(5));
+    }
+    static void factorial1(int x, int output){
+        if (x > 5){
+            System.out.println(output);
+            return;
         }
+        int next_output = output * x;
+        int next_x = x + 1;
+        factorial1(next_x, next_output);
     }
 
+    public static int factorial2(int x) {
+        if (x == 1) {
+            return 1;
+        }
+        int next_x = x - 1;
+        return x * factorial2(next_x);
+    }
 }
