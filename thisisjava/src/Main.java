3a9c6d239d5541e6f7b1021ee4b8b1f65bbdb226
parkseokwon <parkswon1@naver.com>
2024-03-14 17:16:26 +0900

    커밋연습

--- thisisjava/src/Main.java
+++ thisisjava/src/Main.java
@@ -1,23 +1,17 @@
 public class Main {
-    public static void main(String[] args){
-        factorial1(1,1);
-        System.out.println(factorial2(5));
-    }
-    static void factorial1(int x, int output){
-        if (x > 5){
-            System.out.println(output);
-            return;
-        }
-        int next_output = output * x;
-        int next_x = x + 1;
-        factorial1(next_x, next_output);
+    public static void main(String[] args) {
+        double[] array1 = new double[5];
+        double[] array2 = {1.5, 2.4, 3.5};
+
+        System.out.println(array1.length);
+        System.out.println(array2.length);
+
+
+
+
+
+
+        System.out.println(123);
     }
 
-    public static int factorial2(int x) {
-        if (x == 1) {
-            return 1;
-        }
-        int next_x = x - 1;
-        return x * factorial2(next_x);
-    }
 }
--- thisisjava/src/짝수와_홀수_판별기.java
+++ thisisjava/src/짝수와_홀수_판별기.java
@@ -1,8 +1,23 @@
-import java.util.Scanner;
-
 public class 짝수와_홀수_판별기 {
     public static void main(String[] args){
-        Scanner  scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
+        factorial1(1,1);
+        System.out.println(factorial2(5));
+    }
+    static void factorial1(int x, int output){
+        if (x > 5){
+            System.out.println(output);
+            return;
+        }
+        int next_output = output * x;
+        int next_x = x + 1;
+        factorial1(next_x, next_output);
+    }
+
+    public static int factorial2(int x) {
+        if (x == 1) {
+            return 1;
+        }
+        int next_x = x - 1;
+        return x * factorial2(next_x);
     }
 }
