a4cf35c52051eea3af0e40ff8b1f3e571c925de1
parkseokwon <parkswon1@naver.com>
2024-04-11 09:23:54 +0900

    OPP

--- thisisjava/src/OPP.java
+++ thisisjava/src/OPP.java
@@ -1,38 +1,43 @@
+package thisisjava.src;
+
 import java.util.Scanner;
 
-public class Main {
+public class OPP {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("어떤걸 드시겠어요?\n1. 피자 2. 햄버거");
         int orderNum = scanner.nextInt();
         paymentOrder paymentOrder = new paymentOrder();
+        int price = 0;
         switch (orderNum){
             case 1:
                 PizzaOrder pizzaOrder = new PizzaOrder();
-                pizzaOrder.completeOrder();
+                price = pizzaOrder.completeOrder();
                 break;
             case 2:
                 BurgerOrder burgerOrder = new BurgerOrder();
-                burgerOrder.completeOrder();
+                price = burgerOrder.completeOrder();
                 break;
             default:
                 break;
         }
-        paymentOrder.payment(orderNum);
+        paymentOrder.payment(price);
         System.out.println("감사합니다. 안녕히가세요");
     }
 
     public abstract static class Order{
         abstract void prepareFood();
         abstract void serveFood();
+        abstract int priceReturn();
         public void completeOrderPrinter(){
-            System.out.println("주문이 완료되었습니다.");
+            System.out.println("음식을 먹는 중입니다.");
         }
 
-        final void completeOrder(){
+        final int completeOrder(){
             prepareFood();
             serveFood();
             completeOrderPrinter();
+            return priceReturn();
         }
     }
 
@@ -51,6 +56,10 @@ public class Main {
         void serveFood() {
             System.out.println("치즈가 잘늘어나는 피자가 나왔습니다.");
         }
+        @Override
+        int priceReturn(){
+            return 20000;
+        }
     }
 
     public static class BurgerOrder extends Order{
@@ -63,6 +72,10 @@ public class Main {
         @Override
         void serveFood() {
             System.out.println("햄버거에 꽂힌 깃발이 휘날리며 나왔습니다.");
+        }
+        @Override
+        int priceReturn(){
+            return 12000;
         }
     }
 
@@ -82,15 +95,9 @@ public class Main {
     }
 
     public static class paymentOrder{
-        void payment(int caseNum){
-            System.out.println("결제는 어떤걸로 하시겠어요? \n1. 카드 2. 현금");
+        void payment(int price){
+            System.out.println(price + "원 결제는 어떤걸로 하시겠어요? \n1. 카드 2. 현금");
             int paymentNum = scanner.nextInt();
-            int price;
-            if (caseNum == 1){
-                price = 20000;
-            }else {
-                price = 12000;
-            }
 
             switch (paymentNum){
                 case 1:
