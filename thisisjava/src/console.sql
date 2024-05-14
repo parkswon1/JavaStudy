4fe394dc5a90e66a8a896ae3513af22688814613
parkseokwon <parkswon1@naver.com>
2024-05-14 09:38:36 +0900

    db연결

--- thisisjava/src/console.sql
+++ thisisjava/src/console.sql
@@ -1,1 +1,6 @@
-SELECT VERSION();
+CREATE TABLE `users` (
+                         `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                         `name` varchar(255) NOT NULL,
+                         `email` varchar(255) NOT NULL,
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
