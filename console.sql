8e4d84525f8c2ee9a9322f5b67ba5b60725d7ff2
parkseokwon <parkswon1@naver.com>
2024-05-22 16:46:44 +0900

    sql

---
+++ console.sql
@@ -0,0 +1,5 @@
+CREATE TABLE friend (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(255),
+                        email VARCHAR(255)
+);
--- thisisjava/src/console.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `users` (
-                         `id` bigint(20) NOT NULL AUTO_INCREMENT,
-                         `name` varchar(255) NOT NULL,
-                         `email` varchar(255) NOT NULL,
-                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
