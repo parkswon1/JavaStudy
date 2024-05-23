62ff57b67c9c0fc73eb4645887f9f389a4c1a24f
parkseokwon <parkswon1@naver.com>
2024-05-23 14:51:38 +0900

    테이블 추가

--- console.sql
+++ console.sql
@@ -1,5 +1,12 @@
-CREATE TABLE friend (
-                        id SERIAL PRIMARY KEY,
-                        name VARCHAR(255),
-                        email VARCHAR(255)
-);
+INSERT INTO friend (name, email) VALUES
+                                     ('김민수', 'minsu.kim@example.com'),
+                                     ('이하은', 'haeun.lee@example.com'),
+                                     ('박서준', 'seojun.park@example.com'),
+                                     ('최지우', 'jiwoo.choi@example.com'),
+                                     ('정다현', 'dahyun.jung@example.com'),
+                                     ('손예진', 'yejin.son@example.com'),
+                                     ('노준호', 'junho.noh@example.com'),
+                                     ('윤서아', 'seoah.yoon@example.com'),
+                                     ('임태현', 'taehyun.lim@example.com'),
+                                     ('한지안', 'jian.han@example.com'),
+                                     ('조유리', 'yuri.jo@example.com');
