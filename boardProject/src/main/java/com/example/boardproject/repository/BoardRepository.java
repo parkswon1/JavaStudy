3f39903fd719076d6a6fae8c37519683c92037e1
parkseokwon <parkswon1@naver.com>
2024-05-23 16:23:32 +0900

    Board crud 리파지토리 생성

---
+++ boardProject/src/main/java/com/example/boardproject/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.example.boardproject.repository;
+
+import com.example.boardproject.domain.Board;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface BoardRepository extends CrudRepository<Board, Long>, PagingAndSortingRepository<Board, Long> {
+}
